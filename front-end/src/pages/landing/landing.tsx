d48566850f111c14f9b201e98aad94af96699bc0
rolancia <95jjangjun@gmail.com>
2021-10-22 15:32:22 +0900

    feat: [#S05P31B304-127] Landing Page

---
+++ front-end/src/pages/landing/landing.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import LandingTemplate from './template';
+import { CreateStudyBtn } from 'components';
+
+const Landing = () => <LandingTemplate createStudyBtn={<CreateStudyBtn />} />;
+
+export default Landing;
