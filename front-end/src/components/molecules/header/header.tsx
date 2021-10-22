11a359aac219ba52d32562cb232ab2d5303bdb60
rolancia <95jjangjun@gmail.com>
2021-10-22 15:02:51 +0900

    setup: [#S05P31B304-120] common color

--- front-end/src/components/molecules/header/header.tsx
+++ front-end/src/components/molecules/header/header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Layout } from 'antd';
 import { LoginBtn, LogoBtn } from 'components';
 import { CssKeyObject } from 'types/common';
+import 'assets/css/color.css';
 
 const { Header } = Layout;
 
@@ -10,7 +11,7 @@ const styles: CssKeyObject = {
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
-    backgroundColor: 'whitesmoke',
+    backgroundColor: 'var(--header-background)',
     padding: '1em'
   }
 };
