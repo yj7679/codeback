67c3c0a06c3d055e049f178ce0ad50fd5c5d1f63
rolancia <95jjangjun@gmail.com>
2021-10-22 15:32:03 +0900

    feat: [#S05P31B304-126] Landing Page Template

---
+++ front-end/src/pages/landing/template/index.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode } from 'react';
+import { CssKeyObject } from 'types/common';
+
+const styles: CssKeyObject = {
+  container: {
+    height: '90vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+};
+
+type Props = {
+  createStudyBtn: ReactNode;
+};
+
+const LandingTemplate = ({ createStudyBtn }: Props) => (
+  <div style={styles.container}>{createStudyBtn}</div>
+);
+
+export default LandingTemplate;
