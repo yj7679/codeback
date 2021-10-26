b209d7e7a36b18891e51b82feb63d73efbc9af32
rolancia <95jjangjun@gmail.com>
2021-10-26 11:14:10 +0900

    feat: [#S05P31B304-225] page loading component

---
+++ front-end/src/components/atoms/loading/page-loading.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Spin } from 'antd';
+import { CssKeyObject } from 'types/common';
+
+const styles: CssKeyObject = {
+  container: {
+    position: 'absolute',
+    width: '100%',
+    height: '93vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    background: 'rgba(0, 0, 0, 0.2)'
+  }
+};
+
+const PageLoading = () => (
+  <div style={styles.container}>
+    <Spin size="large" />
+  </div>
+);
+
+export default PageLoading;
