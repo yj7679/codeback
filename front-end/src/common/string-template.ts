9a9fb68878db260592c49eef7083459697c643d0
rolancia <95jjangjun@gmail.com>
2021-10-26 11:01:14 +0900

    feat: [#S05P31B304-221] signup 관련 string template

--- front-end/src/common/string-template.ts
+++ front-end/src/common/string-template.ts
@@ -1,1 +1,13 @@
 export const REQUIRE_LOGIN = '로그인이 필요합니다.';
+
+/* signup */
+export const MANDATORY_EMAIL = '이메일은 필수 항목입니다.';
+export const INVALID_EMAIL_FORMAT = '이메일 형식이 올바르지 않습니다.';
+export const DUPLICATED_EMAIL = '이미 사용중인 이메일입니다.';
+
+export const MANDATORY_NICKNAME = '닉네임은 필수 항목입니다.';
+export const NICKNAME_LENGTH_FORMAT = '닉네임은 2자이상 10자 이하';
+export const NICKNAME_CANT_USE_SPECIAL_CHAR = '특수문자 사용 불가능';
+export const DUPLICATED_NICKNAME = '이미 사용중인 닉네임입니다.';
+
+export const INVALID_PASSWORD_FORMAT = '하나의 문자, 숫자 포함 최소 8자 이상 16자 이하';
