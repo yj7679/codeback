157bd48a4c360759a6a50619bca8f55213c7c798
rolancia <95jjangjun@gmail.com>
2021-10-27 16:21:22 +0900

    refactor: [#S05P31B304-230] header

--- front-end/src/app.tsx
+++ front-end/src/app.tsx
@@ -3,11 +3,9 @@ import { BrowserRouter } from 'react-router-dom';
 import './app.css';
 import Router from 'routes/router';
 import { routes } from 'routes/config';
-import { Header } from 'components';
 
 const App = () => (
   <BrowserRouter>
-    <Header />
     <Router routes={routes} />
   </BrowserRouter>
 );
