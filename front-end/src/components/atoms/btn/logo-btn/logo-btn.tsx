1c02b1d8713251e22faa4dd34ec789fdfe7960ae
rolancia <95jjangjun@gmail.com>
2021-11-02 10:36:27 +0900

    style: [#S05P31B304-304] study-header, logo-btn

--- front-end/src/components/atoms/btn/logo-btn/logo-btn.tsx
+++ front-end/src/components/atoms/btn/logo-btn/logo-btn.tsx
@@ -3,14 +3,19 @@ import { Button } from 'antd';
 import { useHistory } from 'react-router-dom';
 import styles from './logo-btn.module.css';
 
-const LogoBtn = () => {
+const LogoBtn = ({ color }: { color?: string }) => {
   const history = useHistory();
   const goToMain = () => {
     history.push('/');
   };
 
   return (
-    <Button className={styles.logoBtn} type="link" size="large" onClick={goToMain}>
+    <Button
+      className={styles.logoBtn}
+      style={color ? { color: color } : {}}
+      type="link"
+      size="large"
+      onClick={goToMain}>
       코드백
     </Button>
   );
