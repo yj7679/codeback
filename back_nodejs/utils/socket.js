307d8cff6df02ee0d089332fe6b9fa7a0793891a
ParkMinsang <whsrpdla@naver.com>
2021-11-03 17:30:12 +0900

    feat: [#S05P31B304-322] node-compileserver

--- back_nodejs/utils/socket.js
+++ back_nodejs/utils/socket.js
@@ -1,29 +1,60 @@
 const socketio = require('socket.io');
+const request = require('request');
 
 module.exports = (server, app) => {
     const io = socketio(server, { path: '/socket.io' });
     app.set('io', io); // req.app.get('io')
 
     io.on('connection', (socket) => { // 웹소켓 연결 시
-        const req = socket.request;
-
         console.log('새 클라이언트 접속 ', socket.id);
+
         socket.on('disconnect', () => {
             console.log('클라이언트 연결 해제 ', socket.id);
-            clearInterval(socket.interval);
         });
 
-        socket.on('error', (error) => {
-            console.error(error);
-        });
-
-        socket.on('reply', (data) => {
+        //사용자를 방에 넣어주는 작업
+        socket.on('join', (data)=>{
             console.log(data);
-        });
+            const {roomId} = data;
+            socket.join(roomId);
+            socket.roomId = roomId;
 
-        socket.interval = setInterval(() => {
-            console.log('emit news');
-            socket.emit('news', 'hello socket.io');
-        }, 3000);
+            socket.to(roomId).emit('join',{
+                'chat' : socket.id + ' is join!'
+            })
+        })
+
+        socket.on('language', (data)=>{
+            const roomId = socket.roomId;
+            const {language} = data;
+
+            if(roomId && language) socket.to(roomId).emit('language', language);
+        } )
+
+        socket.on('compile', (data) => {
+            const roomId = socket.roomId;
+            const {source, input_data, language} = data;
+
+            const compile_data = {
+                script : source,
+                stdin : input_data,
+                language : language,
+                versionIndex: "0",
+                clientId : process.env.JDOODLE_ID,
+                clientSecret : process.env.JDOODLE_SECRET
+            };
+
+            request({
+                url: 'https://api.jdoodle.com/v1/execute',
+                method: "POST",
+                json: compile_data
+            }, (err, resp, body)=>{
+                console.log('err : ', err);
+                console.log('body : ', body);
+
+                if(err == null) socket.to(roomId).emit('compile', body);
+                else socket.to(roomId).emit('compile', err);
+            })
+        });
     });
 }
