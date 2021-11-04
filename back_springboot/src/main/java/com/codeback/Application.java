747a02f29c4347afdaa4a9bb2eddc822069fbde6
이용직 <yj_7679@naver.com>
2021-11-04 13:33:33 +0900

    setup:swaggerHTTPS 8080포트를https 연결

--- back_springboot/src/main/java/com/codeback/Application.java
+++ back_springboot/src/main/java/com/codeback/Application.java
@@ -3,6 +3,9 @@ package com.codeback;
 import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -12,6 +15,12 @@ public class Application {
         SpringApplication.run(Application.class,args);
     }
 
+    @Bean
+    public ServletWebServerFactory serverFactory() {
+        TomcatServletWebServerFactory tocat = new TomcatServletWebServerFactory();
+        tocat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tocat;
+    }
     private Connector createStandardConnector(){
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
         connector.setPort(8070);
