a45eda188e6c43a5faa93cd8c4d9e2244cfb4a4d
rolancia <95jjangjun@gmail.com>
2021-11-05 11:03:27 +0900

    feat: [#S05P31B304-335] logic about signup

--- front-end/src/stores/auth/model/auth-model.ts
+++ front-end/src/stores/auth/model/auth-model.ts
@@ -2,3 +2,10 @@ export type LoginValues = {
   email: string;
   password: string;
 };
+
+export type SignupValues = {
+  email: string;
+  nickname: string;
+  password: string;
+  confirmPassword: string;
+};
--- front-end/src/stores/auth/store/auth.ts
+++ front-end/src/stores/auth/store/auth.ts
@@ -1,14 +1,20 @@
 import { makeAutoObservable, runInAction } from 'mobx';
 import axios, { AxiosError } from 'axios';
 import authRepository from '../repository/auth-repository';
-import { LoginValues } from '../model/auth-model';
+import { LoginValues, SignupValues } from '../model/auth-model';
 import { handleServerError } from 'util/http-error';
-import { FAIL_TO_LOGIN, FAIL_TO_LOGOUT } from 'common/string-template';
+import { FAIL_TO_LOGIN, FAIL_TO_LOGOUT, FAIL_TO_SIGNUP } from 'common/string-template';
 
 export interface Auth {
   authenticated: boolean;
   login: (value: LoginValues) => Promise<void>;
   logout: () => Promise<void>;
+  getSignupStartCookie: () => Promise<void>;
+  checkEmailDuplicated: (email: string) => Promise<void>;
+  checkNicknameDuplicated: (nickname: string) => Promise<void>;
+  sendAuthCode: (email: string) => Promise<void>;
+  confirmAuthCode: (email: string, code: string) => Promise<void>;
+  signup: (values: SignupValues) => Promise<void>;
 }
 
 export class AuthImpl implements Auth {
@@ -44,6 +50,78 @@ export class AuthImpl implements Auth {
         const axiosError = error as AxiosError;
         handleServerError(axiosError);
         throw new Error(FAIL_TO_LOGOUT);
+      }
+    }
+  }
+
+  async getSignupStartCookie() {
+    try {
+      await authRepository.getSignupStartCookie();
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        const axiosError = error as AxiosError;
+        handleServerError(axiosError);
+        throw new Error(FAIL_TO_SIGNUP);
+      }
+    }
+  }
+
+  async checkEmailDuplicated(email: string) {
+    try {
+      await authRepository.checkEmailDuplicated(email);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        const axiosError = error as AxiosError;
+        handleServerError(axiosError);
+        throw new Error(FAIL_TO_SIGNUP);
+      }
+    }
+  }
+
+  async checkNicknameDuplicated(nickname: string) {
+    try {
+      await authRepository.checkNicknameDuplicated(nickname);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        const axiosError = error as AxiosError;
+        handleServerError(axiosError);
+        throw new Error(FAIL_TO_SIGNUP);
+      }
+    }
+  }
+
+  async sendAuthCode(email: string) {
+    try {
+      await authRepository.sendAuthcode(email);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        const axiosError = error as AxiosError;
+        handleServerError(axiosError);
+        throw new Error(FAIL_TO_SIGNUP);
+      }
+    }
+  }
+
+  async confirmAuthCode(email: string, code: string) {
+    try {
+      await authRepository.confirmAuthCode(email, code);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        const axiosError = error as AxiosError;
+        handleServerError(axiosError);
+        throw new Error(FAIL_TO_SIGNUP);
+      }
+    }
+  }
+
+  async signup(values: SignupValues) {
+    try {
+      await authRepository.signup(values);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        const axiosError = error as AxiosError;
+        handleServerError(axiosError);
+        throw new Error(FAIL_TO_SIGNUP);
       }
     }
   }
