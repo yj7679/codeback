57792ccf584eeeabcebb26468ab06a214f9adf0f
rolancia <95jjangjun@gmail.com>
2021-11-18 16:46:35 +0900

    fix: 소켓 두번 연결되는 이슈

--- front-end/src/pages/study/study-rapper.tsx
+++ front-end/src/pages/study/study-rapper.tsx
@@ -1,8 +1,6 @@
 /* eslint-disable */
-
 import React, { useEffect, useState } from 'react';
 import { useLocation, useParams, useHistory } from 'react-router-dom';
-import { observer } from 'mobx-react-lite';
 import { config } from 'config/config';
 import SocketClient from 'config/socket';
 import Study from './study';
@@ -14,29 +12,35 @@ type LocationState = {
   host: boolean;
 };
 
-const StudyRapper = observer(() => {
-  const socket = new SocketClient(config.compileApi);
+let socket: any;
+
+const StudyRapper = () => {
   const location = useLocation<LocationState>();
   const study = useStudy();
+  const history = useHistory();
   const { id }: { id: string } = useParams();
   const [isExistStudy, setIsExistStudy] = useState(false);
-  const history = useHistory();
+
+  console.log(' 몇번 찍히나?');
 
   useEffect(() => {
     study
       .verifyStudy(id)
-      .then(() => setIsExistStudy(true))
+      .then(() => {
+        socket = new SocketClient(config.compileApi);
+        setIsExistStudy(true);
+      })
       .catch((err) => {
         history.push('/');
         msg('Error', err.message);
       });
-  }, [study, id, location.state]);
+  }, []);
 
   if (!isExistStudy) {
     return <PageLoading />;
   }
 
-  return <Study socket={socket} id={id} isHost={location.state ? location.state.host : false}/>;
-});
+  return <Study socket={socket} id={id} isHost={location.state ? location.state.host : false} />;
+};
 
 export default StudyRapper;
